898bd741c9e62bf8c09d85a04ca0c497e728a182
posy-m <minji.posy@gmail.com>
2024-11-15 15:06:56 +0900

    모달 첫번째 수정중

--- src/components/portfolio/Modal.jsx
+++ src/components/portfolio/Modal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from '../../styles/portfolio/modal.module.css';
+import github from '../../images/github2.png'
+import notion from '../../images/notion.png'
 
 function Modal({ isOpen, onClose, content }) {
   if (!isOpen) return null; // 모달이 열려있지 않으면 아무것도 렌더링하지 않음
@@ -9,14 +11,28 @@ function Modal({ isOpen, onClose, content }) {
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <button className={styles.closeButton} onClick={onClose}>X</button>
         <h2>{content.title}</h2>
-        <img src={content.src} alt={content.alt} className={styles.modalImage} />
-        <p>{content.date}</p>
-        <p>{content.person}</p>
-        <p>{content.content}</p>
-        <p>{content.stack}</p>
-        <div>
-          <p>{content.linkName}</p>
-          <img src={content.link} alt="" />
+        <div className={styles.contentWrap}>
+          <div className={styles.modalLeftBox}>
+            <img src={content.src} alt={content.alt} className={styles.modalImage} />
+            <p>{content.date}</p>
+            <a href={content.linkName} target="_blank" rel="noopener noreferrer">
+              <img src={github} alt={content.alt} />
+            </a>
+            <a href={content.linkNameNotion} target="_blank" rel="noopener noreferrer">
+              <img src={notion} alt={content.alt} />
+            </a>
+          </div>
+          <div className={styles.modalRightBox}>
+            <p>{content.subTitle}</p>
+            <p dangerouslySetInnerHTML={{ __html: content.intro }} />
+            <p>{content.stackTitle}</p>
+            <p>{content.stackName}</p>
+            <p>{content.roleTitle}</p>
+            <p>{content.roleName}</p>
+            <p>{content.functionTitle}</p>
+            <p dangerouslySetInnerHTML={{ __html: content.functionName }} />
+            {/* <p>{content.functionName}</p> */}
+          </div>
         </div>
       </div>
     </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,8 @@ import mountin from '../../images/mountin.png'
 import whisky from '../../images/whisky.png'
 import donut from '../../images/donut.png'
 import Modal from './Modal';
+import unmountin from '../../images/unmountin.gif'
+
 
 function Portfolio() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -39,24 +41,38 @@ function Portfolio() {
         <div className={styles.squareWrap}>
           <Square
             date="등산 앱테크"
-            title="24.09.24-24.10.24(30일)"
+            // title="24.09.24-24.10.24(30일)"
             content={"산을 오르며 고도에 따라 포인트를 적립하고, 적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플"}
             src={mountin}
             alt={"이미지"}
             type={"phone"}
             onClick={() => onpenModal({
+              title: "등산 앱테크",
+              src: unmountin,
+              date: "2024.09.24-2024.10.24(30일)",
+              linkName: "https://github.com/posy-m/UPHiLLProjectFront",
+              linkNameNotion: "https://lavish-spike-71b.notion.site/UPHiLL-10bf53737a1380aa8acff5244d02823b",
+              subTitle: "소개",
+              intro: "산을 오르며 고도에 따라 포인트를 적립하고,<br/> 적립 된 포인트로 다양한 상품이나 캐릭터를 구매하여 즐길 수 있는 어플입니다.",
+              stackTitle: "기술스택",
+              stackName: "Nextjs, TypeScript, Tailwind CSS,React-Query, Firebase, Docker, AWS, Jotai",
+              roleTitle: "역할",
+              roleName: "Frontend",
+              functionTitle: "맡은기능",
+              functionName: "사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현 <br/> Jotai를 활용해 회원 정보를 전역 상태에서 불러와 검증한 후, API 요청을 통해 로그인 기능 구현 <br/>Firebase에서 제공하는 Firebase Authentication 서비스를 사용하여 휴대폰 번호로 비밀번호 찾기 / 아이디 찾기 구현",
+              intro: "사용자가 입력하는 데이터를 유효성 검사를 거쳐 회원가입 기능 구현"
 
             })} />
           <Square
             date="주류 커뮤니티 및 추천 사이트"
-            title="24.07.18-24.08.15(18일)"
+            // title="24.07.18-24.08.15(18일)"
             content={"주류를 좋아하는 고객이 게시판을 통해 자유롭게 소통할 수 있는 커뮤니케이션 사이트"}
             src={whisky}
             alt={"이미지"}
             type={"web"} />
           <Square
             date="도넛 커뮤니티"
-            title="24.05.14-24.05.28(14일)"
+            // title="24.05.14-24.05.28(14일)"
             content={"홈페이지에 유입된 고객이 게시판을 통해 자유롭게 소통할 수 있는 커뮤니케이션 사이트"}
             src={donut}
             alt={"이미지"}
